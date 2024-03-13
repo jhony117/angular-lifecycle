52d045e6fc6bcc71d688a62c0489d3c82230a094
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-03-13 10:29:37 -0600

    uso mas commun del onInit y del Ondestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -9,16 +10,20 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   @Input()
   public price : number  = 0 ;
 
-
+//?de usa un $ cuando habalmos de observables
+  public interval$?: Subscription;
 
   ngOnInit(): void {
    console.log('Componente HIJO : ngOnInit');
+
+ this.interval$ =   interval(1000).subscribe(value => console.log(`Tick ${value}`))
     }
   ngOnChanges(changes: SimpleChanges): void {
    console.log('Componente HIJO : ngOnchanges');
     }
   ngOnDestroy(): void {
    console.log('Componente HIJO : ngOnDestroy');
+   this.interval$?.unsubscribe();
     }
 
 
